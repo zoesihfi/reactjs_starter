5c65a5089181293569dcc50d60c3d511c84d557e
zoesihfi <zoemonster919@yahoo.com>
2020-05-20 18:12:09 +0000

    button css and state

--- src/components/PlayGameContainer/PlayGame.js
+++ src/components/PlayGameContainer/PlayGame.js
@@ -12,7 +12,7 @@ export default class ReactRPS extends Component {
             result: ""
             }
         }
-
+    
     render() {
         return(
             <div className = "rps-game">
